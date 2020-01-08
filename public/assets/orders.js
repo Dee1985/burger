757baf12b648bc61bca2eac9259c97a6d222bf38
AyDy Burling <aydy.you@gmail.com>
2020-01-07 18:00:41 -0600

    it's not pretty but it works

--- public/assets/orders.js
+++ public/assets/orders.js
@@ -1,3 +1,29 @@
-
-
- 
+$(document).ready(function() {
+  $.ajax({
+    url: "/allOrders"
+  }).then(function(data) {
+    console.log("burgers front end", data);
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].devoured === 1) {
+        const h3 = $("<h3>").text(data[i].order_text);
+        $(".eaten").append(h3);
+      } else {
+        const h3 = $("<h3>").text(data[i].order_text);
+        const button = $("<button>").text("devour it");
+        button.attr("name", data[i].id);
+        button.attr("class", "devour");
+        $("#orders").append(h3, button);
+      }
+    }
+  });
+  $(document).on("click", ".devour", function() {
+    console.log("you got clicked");
+    $.ajax({
+      url: "/update/" + $(this).attr("name"),
+      type: "PUT"
+    }).then(function(data) {
+      console.log(data);
+      window.location.reload();
+    });
+  });
+});
--- server.js
+++ server.js
@@ -17,25 +17,46 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const posts = [];
-const newPosts = [];
-
+// Setting up EJS
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("orders", { startingContent: homeStartingContent, posts: posts });
+  res.render("orders");
   // console.log(posts);
 });
 
-app.post("/", function(req, res) {
-  // console.log(req.body.burgerName);
-  const post = {
-    name: req.body.burgerName
-  };
-  posts.push(post);
+app.get("/allOrders", (req, res) => {
+  connection.query("SELECT * FROM orders", function(err, results) {
+    res.json(results);
+  });
 });
 
+app.post("/", function(req, res) {
+  console.log("inside / route", req.body.burgerName);
+  const post = {
+    order_text: req.body.burgerName,
+    devoured: false
+  };
+  connection.query(
+    "INSERT INTO orders (order_text, devoured) VALUES (?,?)",
+    [post.order_text, post.devoured],
+    function(err, results) {
+      console.log(err, results);
+    }
+  );
+  // posts.push(post);
+});
+
+app.put("/update/:id", (req, res) => {
+  console.log("about to update", req.params);
+  const sqlCommand = "UPDATE orders SET devoured = ? WHERE id = ?";
+  connection.query(sqlCommand, [true, req.params.id], function(err, results) {
+    console.log(err, results);
+    res.json(results);
+  });
+});
+// ==============================================================================================================================================
 // Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
