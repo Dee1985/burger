7afebab489aad55ec8b8f6db56dda7a0d2c1b0df
AyDy Burling <aydy.you@gmail.com>
2020-01-08 20:36:06 -0600

    connection

--- db/db.js
+++ db/db.js
@@ -1,30 +1,20 @@
 const mysql = require("mysql2");
 
-// var connection;
-// if (process.env.JAWSDB_URL) {
-//   // Database is JawsDB on Heroku
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   // Database is local
-//   connection = mysql.createConnection({
-//     port: 3306,
-//     host: "localhost",
-//     user: "root",
-//     password: "root",
-//     database: "burger_db"
-//   });
-// }
+const connection =
+  mysql.createConnection(process.env.JAWSDB_URL) ||
+  mysql.createConnection({
+    host: "localhost",
 
-// var mysql = require("mysql");
+    // Your port
+    port: 3306,
 
-const connection = mysql.createConnection({
-  host: "rtzsaka6vivj2zp1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "gth62dmtre4psth7",
-  password: "ksr1mmx2ag4md0io",
-  database: "tuy70od0o65ain69",
-  use_env_variable: "JAWSDB_URL"
-});
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "burger_DB"
+  });
 
 connection.connect(function(err) {
   if (err) throw err;
